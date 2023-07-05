b6c6acbe17da32c1031d9f332cd7f59daa931518
Muna Sherpa <munasherpa31@gmail.com>
2023-07-05 15:11:40 +0545

    learn about ejs and config

--- App.js
+++ App.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const app = express();
+const ejs = require('ejs')
 
-app.get('/', (req, res) => {
-    res.send("hello world")
+const {sequelized} = require ('./model/index')
+
+app.set('view engine', 'ejs')
+
+app.get('/login', (req, res) => {
+    // res.send("<h1> I am about page <h1>")
+    res.render('login') // call from views folder indexfile
+})
+
+app.get('/register', (req, res) => {
+    // res.send("<h1> I am about page <h1>")
+    res.render('register') // call from views folder indexfile
 })
 
 app.listen(3000, () => {
